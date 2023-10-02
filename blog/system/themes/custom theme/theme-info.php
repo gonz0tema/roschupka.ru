020766d051322793c02269311902db4499b50cb7
Artyom Roschupka <99813895+gonz0tema@users.noreply.github.com>
2023-10-02 23:21:07 +0300

    Оформление моего блога на Эгее

---
+++ blog/system/themes/custom theme/theme-info.php
@@ -0,0 +1,21 @@
+<?php return array (
+
+  'index' => 3,
+
+  'display_name' => array (
+    'en' => 'Custom Theme',
+    'ru' => 'Кастом',
+  ),
+
+  'colors' => array (
+    'background' => '#fff',
+    'headings' => '#000',
+    'text' => '#000',
+    'link' => '#0060a0',
+  ),
+
+  'based_on' => 'plain',
+  'meta_viewport' => 'width=device-width, initial-scale=1',
+  'supports_dark_mode' => false,
+
+); ?>
